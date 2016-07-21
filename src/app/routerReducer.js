030baeba5526c872471fd0c6a1a26abdd6171490
Carlo DiCelico <carlodicelico@me.com>
2016-07-21 12:41:47 -0400

    make app store an immutable Map; get react-router working with immutable store

---
+++ src/app/routerReducer.js
@@ -0,0 +1,19 @@
+import { Map } from 'immutable';
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+const initialState = Map({
+    locationBeforeTransitions: null
+});
+
+// custom reducer for getting react-router and Immutable to play nice
+const routerReducer = (state = initialState, action) => {
+    if (action.type === LOCATION_CHANGE) {
+        return state.merge({
+            locationBeforeTransitions: action.payload
+        });
+    }
+
+    return state;
+};
+
+export default routerReducer;
