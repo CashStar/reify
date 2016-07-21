2aa8405e569ba4adb77dfffd802af51bb21e4354
Carlo DiCelico <carlodicelico@me.com>
2016-07-21 12:17:22 -0400

    showReportDialog() any time we logException()

--- src/utils/sentryConfig.js
+++ src/utils/sentryConfig.js
@@ -13,6 +13,7 @@ export function logException(ex, context) {
   Raven.captureException(ex, {
     extra: context
   });
+  Raven.showReportDialog();
   /*eslint no-console:0*/
   window && window.console && console.error && console.error(ex);
 }
