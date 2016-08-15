6388f10a6098190a19cc704684d6efdba31e814a
Carlo DiCelico <carlodicelico@me.com>
2016-08-15 13:46:48 -0400

    use latest release candidate of redux-form form immutable compatibility; add todo reducer to root appReducer

--- src/app/appReducer.js
+++ src/app/appReducer.js
@@ -4,13 +4,17 @@ import { reducer as formReducer } from 'redux-form';
 import routerReducer from './routerReducer';
 
 
+import todoReducer from '../todo/todoModule';
+
+
 /*
  * Combine all of our reducers into a root-level reducer
  * This combineReducers() is the redux-immutable one, not the redux one
  */
 const appReducer = combineReducers({
   routing: routerReducer,
-  form: formReducer
+  form: formReducer,
+  todo: todoReducer
 });
 
 export default appReducer;
